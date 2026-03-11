65a4a074518c23c7c6d3e005ddeb4c590d141f68
Riskoder <g.vidal1g+1@gmail.com>
2026-03-11 17:02:01 -0300

    Se creo el modelo para carrito

---
+++ src/main/java/com/mercadolibre_carrito/models/Carrito.java
@@ -0,0 +1,20 @@
+package com.mercadolibre_carrito.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Carrito {
+
+  private Long id;
+
+  private Long userId;
+
+  private List<ProductoCarrito> productos = new ArrayList<>();
+}
